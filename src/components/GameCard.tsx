86173e7e4ca1bdf3c5dccb98b3b620aa37e6d9fa
sina-alizadeh <sina.alizadeh.same@gmail.com>
2023-04-03 18:54:09 +0430

    Some changes about color and box shadow

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,18 +1,16 @@
-import React from "react";
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import Emoji from "./Emoji";
-
 interface Props {
   game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card backgroundColor="">
+    <Card boxShadow="">
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <HStack justifyContent="space-between" marginBottom={3}>
--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -1,13 +1,23 @@
-import React, { ReactNode } from "react";
-import { Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
+import { Box, useColorMode } from "@chakra-ui/react";
 
 interface Props {
   children: ReactNode;
 }
 
 const GameCardContainer = ({ children }: Props) => {
+  const { colorMode } = useColorMode();
   return (
-    <Box borderRadius={10} overflow="hidden">
+    <Box
+      borderRadius={10}
+      overflow="hidden"
+      bg={colorMode === "dark" ? "gray.700" : ""}
+      sx={
+        colorMode === "dark"
+          ? { boxShadow: "0px 0px 8px 0px green" }
+          : { boxShadow: "0px 0px 4px 1px #ccc" }
+      }
+    >
       {children}
     </Box>
   );
